414fc6890bcb3aa4c2a2d8d354529602d7a5321a
chrissetyawan <chrissetyawan@users.noreply.github.com>
2017-04-05 01:07:08 +0700

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -11,6 +11,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
+        // Registered mail
         $this->app->singleton('mailer', function ($app) {
             $app->configure('services');
             $app->configure('mail');
